9b413d169246a69368d094be704c449f91050a5e
flipzky1337 <rorytheloli1337@gmail.com>
2024-06-06 14:00:45 +0500

    little bit of goofy aah fixes and some basic functionality

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import discord
+import config
+import voice_analysis
 from voice_analysis import get_dbfs
+import os
 
-key = 'MTE2NTI1NDMyNzczNzI3NDQwOA.GyzblN.E-iCxZePMTJYQn5Q6jwXE6n8XfSDt6sfSIGrVg'
+key = config.key
 
 bot = discord.Bot()
 connections = {}
 
 
-async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
+# class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+#     @discord.ui.button(label="Остановить запись", style=discord.ButtonStyle.danger) # Create a button with the label "😎 Click me!" with color Blurple
+#     async def button_callback(self, button, interaction):
+#         await interaction.response.defer()
+
+
+async def once_done(sink: discord.sinks, channel: discord.TextChannel, guild_id):
+    server_audio_filepath = f"{config.recording_path}/{guild_id}/"
+
     recorded_users = [
         f"<@{user_id}>"
         for user_id, audio in sink.audio_data.items()
     ]
     await sink.vc.disconnect()
+
+    os.makedirs(os.path.dirname(server_audio_filepath), exist_ok=True)
+
     for user_id, audio in sink.audio_data.items():
-        with open(f"C:/Users/mosina/PycharmProjects/discord-voice-normalization-bot/recordings/{user_id}.mp3",
+        with open(f"{server_audio_filepath}{user_id}.mp3",
                   "wb") as f:
             f.write(audio.file.getbuffer())
 
     # files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
     await channel.send(f"Записал голос для: {', '.join(recorded_users)}.")
-    list_of_responses, sounds = get_dbfs()
+    list_of_responses, sounds = get_dbfs(guild_id)
     for i in range(len(list_of_responses)):
-        await channel.send(f"Уровень в процентах для <@{list_of_responses[i][0]}>: {sounds[i]}" + "%")
+        if isinstance(sounds[i], float):
+            await channel.send(f"Уровень в процентах для <@{list_of_responses[i][0]}>: {sounds[i]}" + "%")
+        else:
+            await channel.send(f'{sounds[i]} <@{list_of_responses[i][0]}>')
 
 
 @bot.event
@@ -56,13 +73,43 @@ async def join(ctx):
     else:
         vc = await voice.channel.connect()
         connections.update({ctx.guild.id: vc})
-        await ctx.respond('Я зашёл, слушаю.')
+
+        async def button_callback(interaction):
+            await interaction.response.defer()
+            await stop_recording(ctx)
+
+        button = discord.ui.Button(label='Остановить запись', style=discord.ButtonStyle.danger, emoji='⛔')
+        view = discord.ui.View(button)
+        button.callback = button_callback
+
+        await ctx.respond('Я зашёл, слушаю.', view=view)
 
         vc.start_recording(
             discord.sinks.MP3Sink(),
             once_done,
-            ctx.channel
+            ctx.channel,
+            ctx.guild.id
         )
+
+
+@bot.slash_command(name='userlist', description='List of users, with percents (recalculated)')
+async def userlist(ctx):
+    try:
+        list_of_responses, sounds = get_dbfs(ctx.guild.id)
+        for i in range(len(list_of_responses)):
+            await ctx.respond('Хорошо! Вот:')
+            await ctx.send(f"Уровень в процентах для <@{list_of_responses[i][0]}>: {sounds[i]}" + "%")
+    except:
+        await ctx.respond('Произошла ошибка, возможно список записей пуст.')
+
+
+@bot.slash_command(name='delete-recording', description='delete specified user\'s recording')
+async def delete_user_audio_recording(ctx, arg):
+    try:
+        voice_analysis.delete_audio(ctx.guild.id, arg)
+        await ctx.respond(f"Аудиоданные удалены для {arg}")
+    except:
+        await ctx.respond(f'Данные для {arg} уже были удалены')
 
 
 bot.run(key)
--- voice_analysis.py
+++ voice_analysis.py
@@ -1,24 +1,25 @@
-# import numpy as np
-# import soundfile as sf
-# from scipy.io.wavfile import read
-#
-# f = sf.SoundFile('295218314400235529.wav')
-# print(f.frames)
-# samprate, wavdata = read('295218314400235529.wav')
+import datetime
 
 from pydub import AudioSegment
-import glob, os, statistics, math
+import glob, os, statistics, math, asyncio
+import config
 
 
-def get_dbfs():
+def get_dbfs(guild_id):
     list = []
     cnt = 1
-    os.chdir('C:/Users/mosina/PycharmProjects/discord-voice-normalization-bot/recordings')
+    guild_recording_path = f'{config.recording_path}/{guild_id}'
+    os.chdir(guild_recording_path)
     for file in glob.glob('*.mp3'):
-        audio = AudioSegment.from_file(file).dBFS
-        list.append((file[0:len(file) - 4], audio))
-        cnt += 1
-        os.remove(file)
+        time_distinction = datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getmtime(file))
+        if time_distinction.days >= 2:
+            os.remove(file)
+        else:
+            audio = AudioSegment.from_file(file).dBFS
+            list.append((file[0:len(file) - 4], audio))
+            cnt += 1
+
+    print(guild_id)
 
     penile = i_hate_math(list)
 
@@ -27,14 +28,14 @@ def get_dbfs():
 
 def i_hate_math(ListDbfs):
     timedlist = [x[1] for x in ListDbfs]
-    print(timedlist)
     array_of_perc = []
+    print(timedlist)
     med = statistics.median(timedlist)
     perc_med = math.pow(10, med / 10)
-    for i in timedlist:
-        j = math.pow(10, i / 10) / perc_med * 100
+    for i in range(len(timedlist)):
+        j = math.pow(10, timedlist[i] / 10) / perc_med * 100
         if 0 <= j <= 200:
-            array_of_perc.append(round(200 - j))
+            array_of_perc.append(200 - j)
         if j > 200:
             array_of_perc.append('Ты слишком громкий, бро.')
         if j < 0:
@@ -42,6 +43,10 @@ def i_hate_math(ListDbfs):
 
     return array_of_perc
 
+
+def delete_audio(guild_id, user_id):
+    user_recording_path = f'{config.recording_path}/{guild_id}/{user_id[2:-1]}.mp3'
+    os.remove(user_recording_path)
 
 # audio = AudioSegment.from_file('295218314400235529.wav')
 # print(audio.dBFS)
